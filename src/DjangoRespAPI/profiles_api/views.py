582cdc47363876438c3b9a3f27cee61a143b910c
shubham-vernekar <vernekar.shubham@gmail.com>
2017-10-31 15:50:30 +0530

    Added search profile function

--- src/DjangoRespAPI/profiles_api/views.py
+++ src/DjangoRespAPI/profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import permissions
 from . import serializers
@@ -106,5 +107,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	queryset = models.UserProfile.objects.all()
 	authentication_classes=(TokenAuthentication,)
 	permission_classes=(permissions.UpdateOwnProfile,)
+	filter_backends=(filters.SearchFilter,)
+	search_fields=('name','email',)
 
 
